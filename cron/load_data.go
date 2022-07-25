c3abdb0e68202c3053ea1a282777e80163b354fc
lk-j <381584494@qq.com>
2022-07-25 22:49:23 +0800

    add antl

--- cron/load_data.go
+++ cron/load_data.go
@@ -1,10 +1,21 @@
 package cron
 
 import (
+	"fmt"
 	"fund_index/db"
 	"fund_index/model"
+	"github.com/panjf2000/ants/v2"
 )
+var pool *ants.PoolWithFunc
+func init()  {
+	pool, _ = ants.NewPoolWithFunc(10, updateMap)
+}
 
+type param struct {
+	code string
+	fundRatio *db.FundRatio
+	fundRatioFormat   *model.FundRatioFormat
+}
 //载入基金净值数据到内存
 func  loadFundRatio()  {
 	//获取基金列表
@@ -14,10 +25,17 @@ func  loadFundRatio()  {
 	//遍历基金列表，获取净值数据，写入内存
 	for _, item := range fundList.GetFundCodeList() {
 		code := item.Code
-		ratioList := fundRatio.GetFundRatioList(code)
-		if code != "" && ratioList !=nil {
-			fundRatioFormat.UpdateMap(code, ratioList)
+		 params := param{
+			code:code,fundRatio:fundRatio,fundRatioFormat:fundRatioFormat,
 		}
-
+		pool.Invoke(params)
 	}
+}
+func updateMap(data interface{})  {
+	params := data.(param)
+	ratioList := params.fundRatio.GetFundRatioList(params.code)
+	if params.code != "" && ratioList !=nil {
+		params.fundRatioFormat.UpdateMap(params.code, ratioList)
+	}
+	fmt.Println(params.code)
 }
